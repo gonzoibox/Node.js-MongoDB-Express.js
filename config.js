fb4da7506f7fbf6009c22b2674cd6b0064bae93e
gonzoibox <gonzoibox@gmail.com>
2019-04-07 11:32:06 +0300

    run localhost/test db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: 3000,
     //MONGO_URL: 'mongodb+srv://hunter:NcnCXf3xnyNfcv#x@cluster0-b2a2z.mongodb.net/test?retryWrites=true'
-    MONGO_URL: 'mongodb://localhost/test-db'
+    MONGO_URL: 'mongodb://localhost/test'
 };
